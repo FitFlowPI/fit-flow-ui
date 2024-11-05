c8b9ff1c12f961aa822c9bbc11f1178b8abe5a44
brcappio <brunocappio@gmail.com>
2024-11-04 21:04:00 -0300

    Created Register, Login and Logout functionalities

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {UserDataComponent} from "./user-data/user-data.component";
+import { HomeComponent } from "./home/home.component";
+import { UserDataComponent } from "./user-data/user-data.component";
+import { GymChartSelectComponent } from "./chart-managing/gym-chart-select/gym-chart-select.component";
+import { authGuard } from './auth.guard'; // Import the guard
+import { LogoutComponent } from './user-data/logout/logout.component';
 
 export const routes: Routes = [
   {
@@ -9,11 +12,21 @@ export const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'logout',
+    component: LogoutComponent,
+  },
+  {
     path: 'user/:actionType',
     component: UserDataComponent,
   },
   {
+    path: 'chart/:chartSection',
+    component: GymChartSelectComponent,
+    canActivate: [authGuard], // Protect this route
+  },
+  {
     path: 'home',
     component: HomeComponent,
+    canActivate: [authGuard], // Protect this route
   }
 ];
