e2624c97253e0ba1052a9a0b1c5bd3c36ed43754
brcappio <brunocappio@gmail.com>
2024-11-20 22:52:28 -0300

    created toast service for login and register

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { ToastModule } from 'primeng/toast';  // Import ToastModule
+import { MessageService } from 'primeng/api'; // Import MessageService
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideHttpClient(), 
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimations(), // Add global animations support
+    MessageService // Provide MessageService globally
   ]
 };
